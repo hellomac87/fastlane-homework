2f0a90a760d27d08ab80c939f8917e5e436aeb03
hellomac87 <hellomac87@gmail.com>
2021-05-20 23:08:01 +0900

    [add] fetch test

--- src/page/home.tsx
+++ src/page/home.tsx
@@ -1,4 +1,10 @@
+import useFetch from "hooks/useFetch";
+
 function Home() {
+  const { status, error, data } = useFetch(
+    `https://fe8eb658-e817-42b3-9c2b-8750cc0b33c4.mock.pstmn.io/latest/ios/products`
+  );
+  console.log({ status, error, data });
   return <div>Home</div>;
 }
 
