380b1dccf1d0ea3c22fd351de5d71c7d8d83fa24
hellomac87 <hellomac87@gmail.com>
2021-05-20 23:24:42 +0900

    [add] price sort

--- src/page/home.tsx
+++ src/page/home.tsx
@@ -1,11 +1,34 @@
 import useFetch from "hooks/useFetch";
+import { Product, ProductsResponseType } from "store/types/products";
 
 function Home() {
-  const { status, error, data } = useFetch(
+  const { status, error, data } = useFetch<ProductsResponseType>(
     `https://fe8eb658-e817-42b3-9c2b-8750cc0b33c4.mock.pstmn.io/latest/ios/products`
   );
-  console.log({ status, error, data });
-  return <div>Home</div>;
+
+  function sortPrice(products: Product[]) {
+    return products.sort(function (a, b) {
+      return b.price - a.price;
+    });
+  }
+
+  if (!data) return <></>;
+  console.log(data.results.recommendProducts);
+  return (
+    <div>
+      <ul>
+        {sortPrice(data.results.recommendProducts).map((product) => (
+          <li key={product.customerCode}>
+            <div>{product.customerName}</div>
+            <div>{product.comment}</div>
+            <div>{product.price}</div>
+            <img src={product.thumbnailImageUrl} alt={product.customerName} />
+            <br />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Home;
--- src/store/types/products.ts
+++ src/store/types/products.ts
@@ -1,3 +1,14 @@
+export type ProductsResponseType = {
+  results: {
+    imgPath: {};
+    newProducts: Product[];
+    recommendProducts: Product[];
+    yeoshinTv: YeoshinTv[];
+  };
+  serverCode: string;
+  serverMsg: string;
+};
+
 export type Product = {
   locationName: string;
   distance?: string;
@@ -16,4 +27,17 @@ export type Product = {
   name: string;
   comment: string;
   wishCount: number;
+};
+
+type YeoshinTv = {
+  tvCode: "S0351298";
+  tvId: "95";
+  tvImgType: "direct";
+  tvInputDate: "2021-04-02 13:23:16.0";
+  tvMetatagTitle: "";
+  tvNameMain: "[카프리레이저] 여드름, 뿌리부터 뽑아야 좋아지지~";
+  tvVideoId: "Gmv4l-_Z6MI";
+  tvVideoThumb: "https://d10fvx8ndeqwvu.cloudfront.net/upfiles/product/2322908901.jpg?w=320&amp;h=180&amp;f=webp&amp;q=90";
+  tvVideoUrl: "https://youtu.be/Gmv4l-_Z6MI";
+  tvViewCount: "2394";
 };
