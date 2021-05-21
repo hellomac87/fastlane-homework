852f560ec2115bc0226120342e1e4db4cf361f0d
hellomac87 <hellomac87@gmail.com>
2021-05-21 20:09:11 +0900

    [fix] globalstyle

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,28 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import GlobalStyle from "styles/GlobalStyle";
+
 import { Provider } from "react-redux";
 import { store } from "store";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const GlobalStyle = createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap');
+    ${reset};
+    
+    * {
+        margin: 0;
+        padding: 0;
+        outline: 0;
+        box-sizing: border-box;   
+    }
+    body{
+      background-color:#ffffff;
+      font:10px 'Roboto', sans-serif;
+    }
+`;
+
 ReactDOM.render(
   <>
     <GlobalStyle />
